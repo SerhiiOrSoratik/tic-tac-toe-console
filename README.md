67e2c34c03a0de0afea893477ee8cd49a975f586
SerhiiOrSoratik <87651890+SerhiiOrSoratik@users.noreply.github.com>
2021-07-27 11:35:53 +0300

    tic-tac-toe console

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tic-tac-toe-console
+# tic-tac-toe-console
--- server.js
+++ server.js
@@ -1,150 +1,119 @@
-const { throws } = require('assert/strict');
 const net = require('net');
-const stream = require('stream');
 
 let players = [];
 let userOnline = 0;
+let nextMove = {};
+let field = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ];
+
 const server = net.createServer(socket => {
 
-    const clientInfo = `${socket.remoteAddress}:${socket.remotePort}`;
-    console.log(`+ ${clientInfo} - connected`);
-    userOnline++;
-
-    if (userOnline === 1) {
-        players.push({ id: socket.remotePort, symbol: 'x', move: 1 });
+    const startGame = () => {
         field = [
             [" ", " ", " "],
             [" ", " ", " "],
             [" ", " ", " "],
           ];
-        socket
-          .pipe(new ticTacToe(field, socket, players)) // convert client messages to upper case
-          .pipe(socket); 
+          show(field);
+    }
+
+    const playerTurn = (position, field) => {
+        let pos = position.split(' ');
+        const x = Number(pos[0]);
+        const y = Number(pos[1]);
+        if(x <= 2 && x >= 0 && y >= 0 && y <= 2) {
+        if (field[x][y] === ' ') {
+            field[x][y] = nextMove.symbol;
+            show(field);
+            if (checkConditionGame(field, nextMove.symbol)) {
+                nextMove = {
+                    socket: nextMove.socket === players[0].socket ? players[1].socket : players[0].socket,
+                    symbol: nextMove.symbol === 'x' ? 'o' : 'x'
+                }
+                players.map(player => {
+                    if (player.socket === nextMove.socket) {
+                        player.socket.write('You move');
+                        player.socket.write('\n')
+                    }
+                }); 
+            } else {
+                players.map(player => {
+                    if (player.socket === nextMove.socket) {
+                        player.socket.write('YOU WIN')
+                    }
+                    else {
+                        player.socket.write('YOU LOSE');
+                    }
+                })
+                startGame();
+            }
+        }
+    }
+    }
+
+    const checkConditionGame = (field, symbol) => {
+        if (field[0].includes(' ') === false && field[1].includes(' ') === false && field[2].includes(' ') === false) {
+            console.log('DRAW!');
+            return false;
+        } else {
+            return !(field[0][0] === symbol && field[1][0] === symbol && field[2][0] === symbol ||
+                field[0][1] === symbol && field[1][1] === symbol && field[2][1] === symbol ||
+                field[0][2] === symbol && field[1][2] === symbol && field[2][2] === symbol ||
+                field[0][0] === symbol && field[0][1] === symbol && field[0][2] === symbol ||
+                field[1][0] === symbol && field[1][1] === symbol && field[1][2] === symbol ||
+                field[2][0] === symbol && field[2][1] === symbol && field[2][2] === symbol ||
+                field[0][0] === symbol && field[1][1] === symbol && field[2][2] === symbol ||
+                field[0][2] === symbol && field[1][1] === symbol && field[2][0] === symbol);
+        }
+    }
+    
+    const show = (field) => {
+        let fieldView = '';
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if ((i === 1 && j === 1) || (i === 2 && j === 1) || (i === 0 && j === 1)) {
+                  fieldView += "| " + field[i][j] + " |";
+                } else {
+                  fieldView += " " + field[i][j];
+                }
+            }
+                if (i !== 2) {
+                    fieldView += '\n--+---+--\n';
+                }
+            }
+            players.map(player => {
+                player.socket.write('\n')
+                player.socket.write(fieldView)
+                player.socket.write('\n')
+            })
+        }
+
+    userOnline++;
+
+    if (userOnline === 1) {
+        players.push({ socket: socket, symbol: 'x', move: 1 });
     }
     else if (userOnline === 2) {
-        players.push({ id: socket.remotePort, symbol: 'o', move: 2 });
-        
-          socket
-          .pipe(new ticTacToe(field, socket, players)) // convert client messages to upper case
-          .pipe(socket); // forwards uppercased text back to client
+        players.push({ socket: socket, symbol: 'o', move: 2 });
+        startGame();
     } else if (userOnline > 2) {
         socket.disconnect(true);
         userOnline--;
     }
-    console.log("online user: " + userOnline);
 
-    socket.write('Echo server\r\n');
-    
     socket.on('data', message => {
-        process.stdout.write(`> ${clientInfo} : ${message}`);
+        if (nextMove.socket === undefined) {
+            nextMove = {socket: socket, symbol: players.find(player => player.socket === socket).symbol};
+        } 
+        if (socket === nextMove.socket) {
+            playerTurn(message.toString(), field);
+        }
+        
     })
-
-    socket.on('close', () => {
-        console.log(`- ${clientInfo} - closed`)
-    })
+    
 });
 
 server.listen(1337, '10.0.0.227');
-
-class ticTacToeGame {
-    field = [
-        [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "],
-      ];
-    players;
-    constructor(player1, player2) {
-        this.players.push(player1);
-        this.players.push(player2);
-    }
-
-    
-}
-
-class Player {
-    socketIp;
-    moveType;
-    constructor(socketIp, moveType) {
-        this.socketIp = socketIp;
-        this.moveType = moveType;
-    }
-
-}
-
-// class ticTacToe extends stream.Transform {
-//     field;
-//     socket;
-//     players;
-//     userOnline = 0;
-//     lastMove = 0;
-
-//     constructor(field, socket, players) {
-//         super()
-//         this.field = field;
-//         this.socket = socket;
-//         this.players = players;
-//     }
-
-//     _transform(chunk, enc, done) {
-//         this.move('0 0')
-//         this.push(this.socket.id)
-//         done();
-//     }
-
-//     move(data) {
-//         if (this.lastMove !== this.players.find((pl) => pl.id === socket.remoteAddress).move) {
-//             const position = data.position.split(' ');
-    
-//             if (position[0] >= 0 && position[0] <= 2 && position[1] >= 0 && position[1] <= 2) {
-//                const x = position[1];
-//                const y = position[0];
-//                 if (field[x][y] === ' ') {
-                        
-//                         field[x][y] = players.find((pl) => pl.id === socket.remoteAddress).symbol.toString();
-//                         io.sockets.emit("move complete", {field: field, nextMove: lastMove === 1 ? 'o' : 'x'});     
-//                         checkConditionGame(field, players.find((pl) => pl.id === socket.remoteAddress).symbol.toString());
-//                         lastMove = players.find((pl) => pl.id === socket.remoteAddress).move;
-                     
-//                 }
-//                 else {
-//                      console.log('wrong move')
-//                 }
-//             }
-//         }
-//     }
-
-//     show(chunk) {
-//         let fieldView = "";
-//         for (let i = 0; i < 3; i++) {
-//           for (let j = 0; j < 3; j++) {
-//             if ((i === 1 && j === 1) || (i === 2 && j === 1) || (i === 0 && j === 1)) {
-//               fieldView += "| " + this.field[i][j] + " |";
-//             } else {
-//               fieldView += " " + this.field[i][j];
-//             }
-//           }
-//           if (i !== 2) {
-//             fieldView += "\n--+---+--\n";
-//           }
-//         }
-//         console.clear();
-//         console.log(fieldView)
-//     }
-
-//     checkConditionGame(symbol) {
-//         if (field[0].includes(" ") === false && field[1].includes(" ") === false && field[2].includes(" ") === false) {
-//             return "Draw!";
-//       } else if (
-//             (this.field[0][0] === symbol && this.field[1][0] === symbol && this.field[2][0] === symbol) ||
-//             (this.field[0][1] === symbol && this.field[1][1] === symbol && this.field[2][1] === symbol) ||
-//             (this.field[0][2] === symbol && this.field[1][2] === symbol && this.field[2][2] === symbol) ||
-//             (this.field[0][0] === symbol && this.field[0][1] === symbol && this.field[0][2] === symbol) ||
-//             (this.field[1][0] === symbol && this.field[1][1] === symbol && this.field[1][2] === symbol) ||
-//             (this.field[2][0] === symbol && this.field[2][1] === symbol && this.field[2][2] === symbol) ||
-//             (this.field[0][0] === symbol && this.field[1][1] === symbol && this.field[2][2] === symbol) ||
-//             (this.field[0][2] === symbol && this.field[1][1] === symbol && this.field[2][0] === symbol)) {
-//             console.log("Win!");
-//            return "Winner: " + symbol;
-//         }
-//     }
-// }
